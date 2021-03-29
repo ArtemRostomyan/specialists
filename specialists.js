2fca3a111ce5ac1793d19c717bbd8abc4178d361
ArtemRostomyan <71639600+ArtemRostomyan@users.noreply.github.com>
2021-03-29 13:47:46 +0400

    Update specialists.js

--- specialists.js
+++ specialists.js
@@ -2,16 +2,112 @@
     {
         "id": "spec-1",
         "name": "Швея",
-        "price": 2700
+        "price": 2700,
+        "normativ_price_bed": [
+            {
+                "name": "small",
+                "price": 0.4
+            },
+            {
+                "name": "middle",
+                "price": 0.3
+            },
+            {
+                "name": "big",
+                "price": 0.5
+            }
+        ],
+        "normativ_price_sofa": [
+            {
+                "name": "super_small",
+                "price": 0.3
+            },
+            {
+                "name": "small",
+                "price": 1
+            },
+            {
+                "name": "middle",
+                "price": 1.5
+            },
+            {
+                "name": "big",
+                "price": 2
+            }
+        ]
     },
     {
         "id": "spec-2",
         "name": "Обивщик",
-        "price": 2700
+        "price": 2700,
+        "normativ_price_bed": [
+            {
+                "name": "small",
+                "price": 0.5
+            },
+            {
+                "name": "middle",
+                "price": 0.6
+            },
+            {
+                "name": "big",
+                "price": 0.7
+            }
+        ],
+        "normativ_price_sofa": [
+            {
+                "name": "super_small",
+                "price": 0.3
+            },
+            {
+                "name": "small",
+                "price": 1
+            },
+            {
+                "name": "middle",
+                "price": 1.2
+            },
+            {
+                "name": "big",
+                "price": 1.5
+            }
+        ]
     },
     {
         "id": "spec-3",
         "name": "Каркасник",
-        "price": 3100
+        "price": 3100,
+        "normativ_price_bed": [
+            {
+                "name": "small",
+                "price": 0.2
+            },
+            {
+                "name": "middle",
+                "price": 0.5
+            },
+            {
+                "name": "big",
+                "price": 0.5
+            }
+        ],
+        "normativ_price_sofa": [
+            {
+                "name": "super_small",
+                "price": 0.2
+            },
+            {
+                "name": "small",
+                "price": 0.5
+            },
+            {
+                "name": "middle",
+                "price": 0.5
+            },
+            {
+                "name": "big",
+                "price": 0.5
+            }
+        ]
     }
 ]
