aeb8dd6abb2d2824d80c01cf2f4387a8f677b79c
ArtemRostomyan <71639600+ArtemRostomyan@users.noreply.github.com>
2021-07-11 20:06:18 +0500

    Update specialists.js

--- specialists.js
+++ specialists.js
@@ -19,18 +19,22 @@
         ],
         "normativ_price_sofa": [
             {
+                "id": 1,
                 "name": "super_small",
                 "price": 0.3
             },
             {
+                "id": 2,
                 "name": "small",
                 "price": 1
             },
             {
+                "id": 3,
                 "name": "middle",
                 "price": 1.5
             },
             {
+                "id": 4,
                 "name": "big",
                 "price": 2
             }
@@ -56,18 +60,22 @@
         ],
         "normativ_price_sofa": [
             {
+                "id": 1,
                 "name": "super_small",
                 "price": 0.3
             },
             {
+                "id": 2,
                 "name": "small",
                 "price": 1
             },
             {
+                "id": 3,
                 "name": "middle",
                 "price": 1.2
             },
             {
+                "id": 4,
                 "name": "big",
                 "price": 1.5
             }
@@ -93,18 +101,22 @@
         ],
         "normativ_price_sofa": [
             {
+                "id": 1,
                 "name": "super_small",
                 "price": 0.2
             },
             {
+                "id": 2,
                 "name": "small",
                 "price": 0.5
             },
             {
+                "id": 3,
                 "name": "middle",
                 "price": 0.5
             },
             {
+                "id": 4,
                 "name": "big",
                 "price": 0.5
             }
